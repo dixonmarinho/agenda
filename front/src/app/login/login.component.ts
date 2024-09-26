d44affb23f3594753e5f56155c3e0931ef53e491
dixonmarinho <85615406+dixonmarinho@users.noreply.github.com>
2024-09-26 20:23:44 -0300

    Modifications

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,12 +1,28 @@
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,  ReactiveFormsModule, CommonModule, MatInputModule, MatSelectModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  hide = true;
+  formControl = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  senha = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Você deve inserir um valor';
+    }
+    return this.email.hasError('email') ? 'Não é um email válido' : '';
+  }
 }
